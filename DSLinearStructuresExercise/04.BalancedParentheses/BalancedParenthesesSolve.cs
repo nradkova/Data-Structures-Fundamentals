9de18a670cac86554140567a3a6ddd173a563b70
nradkova <neliradkovaa@gmail.com>
2021-03-01 19:25:32 +0200

    OpenCourse

---
+++ DSLinearStructuresExercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,63 @@
+﻿namespace Problem04.BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            return Solve(parentheses);
+        }
+
+        private bool Solve(string parentheses)
+        {
+            var stack = new Stack<char>();
+            for (int i = 0; i < parentheses.Length; i++)
+            {
+                var current = parentheses[i];
+                if (current == '('
+                    || current == '{'
+                    || current == '[')
+                {
+                    stack.Push(current);
+                }
+
+                if (stack.Count > 0)
+                {
+                    var prev = stack.Peek();
+                    if (isValid(current, prev))
+                    {
+                        stack.Pop();
+                    }
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool isValid(char current, char prev)
+        {
+            if (current == '}' && prev == '{')
+            {
+                return true;
+            }
+            if (current == ')' && prev == '(')
+            {
+                return true;
+            }
+            if (current == ']' && prev == '[')
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
