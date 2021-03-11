1465b5f1e3ec21ae55f10197eb88fa4f3065f73a
nradkova <neliradkovaa@gmail.com>
2021-03-11 23:22:29 +0200

    OpenCourse

---
+++ DSHeapsExercise/04.CookiesProblem/CookiesProblem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Wintellect.PowerCollections;
+
+namespace _04.CookiesProblem
+{
+    public class CookiesProblem
+    {
+        public int Solve(int k, int[] cookies)
+        {
+            var bag = new OrderedBag<int>();
+            foreach (var cookie in cookies)
+            {
+                bag.Add(cookie);
+            }
+            int currentCookie = bag.GetFirst();
+            int count = 0;
+            while (currentCookie<k
+                && bag.Count>1)
+            {
+                int firstCookie = bag.RemoveFirst();
+                int secondCookie = bag.RemoveFirst();
+                int combined = firstCookie + 2 * secondCookie;
+                bag.Add(combined);
+                currentCookie = bag.GetFirst();
+                count++;
+            }
+            return currentCookie < k ? -1 : count;
+        }
+    }
+}
