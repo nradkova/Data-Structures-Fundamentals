6d396576e640dbb3f7392a88554d07aa27d741c7
nradkova <neliradkovaa@gmail.com>
2021-03-16 21:24:46 +0200

    OpenCourse

---
+++ DSTreeTraversalLab/02.BreadthFirstOrder/Tree.cs
@@ -0,0 +1,81 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private readonly List<Tree<T>> _children;
+
+        public Tree(T value)
+        {
+            this.Value = value;
+            this.Parent = null;
+            this._children = new List<Tree<T>>();
+        }
+
+        public Tree(T value, params Tree<T>[] children)
+            : this(value)
+        {
+            foreach (var child in children)
+            {
+                child.Parent = this;
+                this._children.Add(child);
+            }
+        }
+
+        public T Value { get; private set; }
+        public Tree<T> Parent { get; private set; }
+        public IReadOnlyCollection<Tree<T>> Children
+            => this._children.AsReadOnly();
+
+        public ICollection<T> OrderBfs()
+        {
+            var result = new List<T>();
+            var queue = new Queue<Tree<T>>();
+            var node = this;
+            queue.Enqueue(node);
+            while (queue.Count > 0)
+            {
+                var subTree = queue.Dequeue();
+                result.Add(subTree.Value);
+                foreach (var child in subTree.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+            return result;
+        }
+
+        public ICollection<T> OrderDfs()
+        {
+            var result = new List<T>();
+            OrderDfs(this, result);
+            return result;
+        }
+
+        private void OrderDfs(Tree<T> tree, List<T> result)
+        {
+            foreach (var child in tree.Children)
+            {
+                OrderDfs(child, result);
+            }
+                result.Add(tree.Value);
+        }
+
+        public void AddChild(T parentKey, Tree<T> child)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveNode(T nodeKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Swap(T firstKey, T secondKey)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
