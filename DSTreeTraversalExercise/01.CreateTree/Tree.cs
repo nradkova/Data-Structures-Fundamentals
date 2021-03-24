1f2fdbc6168bfde07aab8ebfea7f7c0bd6b8213d
nradkova <neliradkovaa@gmail.com>
2021-03-25 00:01:07 +0200

    OpenCourse

---
+++ DSTreeTraversalExercise/01.CreateTree/Tree.cs
@@ -0,0 +1,89 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private readonly List<Tree<T>> _children;
+
+        public Tree(T key, params Tree<T>[] children)
+        {
+            this.Key = key;
+            this._children = new List<Tree<T>>();
+            foreach (var child in children)
+            {
+                this._children.Add(child);
+            }
+        }
+
+        public T Key { get; private set; }
+
+        public Tree<T> Parent { get; private set; }
+
+
+        public IReadOnlyCollection<Tree<T>> Children
+            => this._children.AsReadOnly();
+
+        public void AddChild(Tree<T> child)
+        {
+            this._children.Add(child);
+        }
+
+        public void AddParent(Tree<T> parent)
+        {
+            this.Parent = parent;
+        }
+
+        public string GetAsString()
+        {
+            StringBuilder result = new StringBuilder();
+            int indent = 0;
+            this.FindDFS(this, indent, result);
+            return result.ToString().Trim();
+        }
+
+        private void FindDFS(Tree<T> subTtree, int indent,
+            StringBuilder result)
+        {
+            result.Append(' ', indent);
+            result.AppendLine(subTtree.Key.ToString());
+
+            foreach (var child in subTtree.Children)
+            {
+                FindDFS(child, indent + 2, result);
+            }
+        }
+
+        public Tree<T> GetDeepestLeftomostNode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetLeafKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetMiddleKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetLongestPath()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<List<T>> PathsWithGivenSum(int sum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tree<T>> SubTreesWithGivenSum(int sum)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
