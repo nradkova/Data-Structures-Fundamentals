757260e95d3c2b7dbca7cd6ae5326e581e7e3865
nradkova <neliradkovaa@gmail.com>
2021-04-06 11:11:32 +0300

    ExamPreparation

---
+++ DSExamPrep/01.Loader/Loader.cs
@@ -0,0 +1,159 @@
+﻿namespace _01.Loader
+{
+    using _01.Loader.Interfaces;
+    using _01.Loader.Models;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Loader : IBuffer
+    {
+        private List<IEntity> entities;
+        public Loader()
+        {
+            this.entities = new List<IEntity>();
+        }
+        public int EntitiesCount => this.entities.Count;
+
+        //O(1)amortized
+        public void Add(IEntity entity)
+        {
+            this.entities.Add(entity);
+        }
+
+        public void Clear()
+        {
+            this.entities.Clear();
+        }
+
+        public bool Contains(IEntity entity)
+        {
+            return this.GetById(entity.Id) != null;
+        }
+
+        public IEntity Extract(int id)
+        {
+            var current = this.GetById(id);
+            if (current!=null)
+            {
+                this.entities.Remove(current);
+            }
+            return current;
+        }
+
+        public IEntity Find(IEntity entity)
+        {
+            return this.GetById(entity.Id);
+        }
+
+        public List<IEntity> GetAll()
+        {
+            return new List<IEntity>(this.entities);
+        }
+
+        public IEnumerator<IEntity> GetEnumerator()
+        {
+            foreach (var entity in entities)
+            {
+                yield return entity;
+            }
+        }
+
+        public void RemoveSold()
+        {
+            var temp = new List<IEntity>();
+            foreach (var item in temp)
+            {
+                if (!item.Status.Equals(BaseEntityStatus.Sold))
+                {
+                    temp.Add(item);
+                }
+            }
+            this.entities = temp;
+        }
+
+        public void Replace(IEntity oldEntity, IEntity newEntity)
+        {
+           int index= this.ValidateEntityIndex(oldEntity);
+            entities[index] = newEntity;
+        }
+
+        public List<IEntity> RetainAllFromTo
+            (BaseEntityStatus lowerBound, BaseEntityStatus upperBound)
+        {
+            int lower =(int) lowerBound;
+            int upper = (int)upperBound;
+            var list = new List<IEntity>(this.EntitiesCount);
+            foreach (var entity in entities)
+            {
+                if (IsStatusInRange(entity.Status,lower,upper))
+                {
+                    list.Add(entity);
+                }
+            }
+            return list;
+        }
+
+        public void Swap(IEntity first, IEntity second)
+        {
+            int firstIndex = this.ValidateEntityIndex(first);
+            int secondIndex = this.ValidateEntityIndex(second);
+            entities[firstIndex] = second;
+            entities[secondIndex] = first;
+        }
+
+        public IEntity[] ToArray()
+        {
+            return this.entities.ToArray();
+        }
+
+        public void UpdateAll(BaseEntityStatus oldStatus, BaseEntityStatus newStatus)
+        {
+            foreach (var item in this.entities)
+            {
+                if ((BaseEntityStatus)item.Status==(BaseEntityStatus)oldStatus)
+                {
+                    item.Status=(BaseEntityStatus)newStatus;
+                }
+            }
+        }
+
+        private bool IsStatusInRange(BaseEntityStatus status,
+            int lower, int upper)
+        {
+            if ((int)status < lower)
+            {
+                return false;
+            }
+            if ((int)status > upper)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private int ValidateEntityIndex(IEntity oldEntity)
+        {
+            int index = this.entities.IndexOf(this.GetById(oldEntity.Id));
+            if (index == -1)
+            {
+                throw new InvalidOperationException("Entity not found");
+            }
+            return index;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        => this.GetEnumerator();
+
+        private IEntity GetById(int id)
+        {
+            foreach (var entity in entities)
+            {
+                if (entity.Id==id)
+                {
+                    return entity;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ DSExamPrep/02.Data/Data.cs
@@ -0,0 +1,117 @@
+﻿namespace _02.Data
+{
+    using _02.Data.Interfaces;
+    using _02.Data.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Wintellect.PowerCollections;
+
+    public class Data : IRepository
+    {
+        private OrderedBag<IEntity> entities;
+
+        public Data()
+        {
+            entities = new OrderedBag<IEntity>();
+        }
+        public Data(Data copy)
+        {
+            this.entities = copy.entities;
+        }
+
+        public int Size => this.entities.Count;
+       
+        public void Add(IEntity entity)
+        {
+            entities.Add(entity);
+            var parent = this.GetById((int)entity.ParentId);
+            if (parent!=null)
+            {
+                parent.AddChild(entity);
+            }
+        }
+
+        public IRepository Copy()
+        {
+            Data result =(Data)this.MemberwiseClone();
+            return new Data( result);
+        }
+
+        public IEntity DequeueMostRecent()
+        {
+            this.EnsureNotEmpty();
+            return this.entities.RemoveFirst();
+        }
+
+        public List<IEntity> GetAll()
+        {
+            return new List<IEntity>(this.entities);
+        }
+
+        public List<IEntity> GetAllByType(string type)
+        {
+            if (type!= typeof(Invoice).Name
+                && type != typeof(StoreClient).Name
+                  && type != typeof(User).Name)
+            {
+                throw new InvalidOperationException
+                    ($"Invalid type: {type}");
+            }
+            var result = new List<IEntity>();
+            foreach (var entity in entities)
+            {
+                if (entity.GetType().Name==type)
+                {
+                    result.Add(entity);
+                }
+            }
+            return result;
+        }
+
+        public IEntity GetById(int id)
+        {
+            if (id<0||id>=this.Size)
+            {
+                return null;
+            }
+            return entities[this.Size-1-id];
+        }
+
+        public List<IEntity> GetByParentId(int parentId)
+        {
+            var parent = this.GetById(parentId);
+            return parent != null ? parent.Children : new List<IEntity>();
+        }
+
+        public IEntity PeekMostRecent()
+        {
+            this.EnsureNotEmpty();
+            return this.entities.GetFirst();
+        }
+
+        private int CompareElements(IEntity x, IEntity y)
+        {
+            return y.Id - x.Id;
+        }
+
+        private void EnsureNotEmpty()
+        {
+            if (this.Size == 0)
+            {
+                throw new InvalidOperationException
+                    ("Operation on empty Data");
+            }
+        }
+        
+        private List<string> GetCustomTypes()
+        {
+            var result = new HashSet<string>();
+            foreach (var entity in entities)
+            {
+                result.Add(entity.GetType().Name);
+            }
+            return result.ToList();
+        }
+    }
+}
